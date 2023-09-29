b1e4c9a1da074fa4125677b2141b5e3da8c86782
Mei0506 <130352983+Mei0506@users.noreply.github.com>
2023-09-29 18:06:12 +0000

    [add]admin_items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,32 @@
-class Public::ItemsController < ApplicationController
-  def index
-  end
-
+class Admin::ItemsController < ApplicationController
+  
   def new
+    @items = Item.new
   end
-
-  def create
-  end
-
+  
   def show
+    @item = Item.find(params[:id])
   end
-
+  
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def index
+    @items = Item.all.page(params[:page]).per(10)
+  end
+  
+  def create
+    @item = Item.new(item_params)
   end
   
   def update
+    @item = Item.find(params[:id])
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_selling, :image)
+  end
+  
 end
