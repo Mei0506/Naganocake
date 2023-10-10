bb9a73bb7d4375dc1a6f3be1a47eba0eb5047177
Mei0506 <130352983+Mei0506@users.noreply.github.com>
2023-10-10 10:39:59 +0000

    [完了]管理者：商品編集画面まで

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,18 +20,26 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.save
       flash[:notice] = "商品の新規登録に成功しました。"
-      redirect_to admin_items_path
+      redirect_to admin_item_path(@item.id)
     else
-      flash.now[:notice] = "商品の新規登録に失敗しました。"
+      flash.now[:notice] = "もう一度入力してください。"
       render :new
     end
   end
   
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      flash[:notice] = "登録情報を変更しました。"
+      redirect_to admin_item_path(@item.id)
+    else
+      flash.now[:notice] = "もう一度入力してください。"
+      render :edit
+    end
   end
+  
+  
 
   private
   def item_params
